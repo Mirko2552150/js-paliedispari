381c2e7fb242011898cfacf2450cad680a1a8d75
Mirko <mirko.longo.1987@gmail.com>
2020-02-24 17:05:27 +0100

    Svolgimento compito

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+// COMPITO UNO
+// Una funzione per capire se la parola è palindroma
+// COMPITO UNO SVOLGIMENTO:
+// 1) far inserire una parola
+// 2) controllare con un funzione se è palindroma
+// 3) suggerimento, ordine lettura, se === allora palindroma
+
+
+// COMPITO DUE
+// L’utente sceglie pari o dispari e un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer.
+// Confrontiamo i due numeri e dichiariamo chi ha vinto.
+// COMPITO DUE SVOLGIMENTO:
+// 1) con 2 prompt inserire i numeri con funzione RANDOM da 1 a 5(esempio lezione)
+// 2) confronto numeri
+// 3) risultante chi ha vinto
